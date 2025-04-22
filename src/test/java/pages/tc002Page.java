655d8cc26a5b67ae6384160c57588e86f5d24e5b
Maximiliano Reyes López <113549509+mmaaxi@users.noreply.github.com>
2025-04-21 19:14:01 -0600

    agregando src/test/java/pages/tc002Page.java

---
+++ src/test/java/pages/tc002Page.java
@@ -0,0 +1,20 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class tc002Page {
+    WebDriver driver;
+
+    public tc002Page(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void ejecutarAccion2() {
+        // Implementar la lógica para ejecutar la acción 2
+    }
+
+    public boolean verificarResultado2() {
+        // Implementar la lógica para verificar el resultado 2
+        return true; // Cambiar el valor de retorno basado en la verificación
+    }
+}
